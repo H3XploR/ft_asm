fce3e7eff9046f52623d5e03d0557824c9e5912d
hexplor <hexplor@hexplor.com>
2026-04-06 05:03:14 +0200

    reset de ft_write.s

--- ft_write_dir/ft_write.s
+++ ft_write_dir/ft_write.s
@@ -6,11 +6,3 @@ section .text
 extern __errno_location
 global ft_write
 
-ft_write:
-	mov	rax, 1
-	syscall
-	cmp	rax, -1
-	je	err
-
-err:
-	call __errno_location
--- ft_write_dir/test.c
+++ ft_write_dir/test.c
@@ -1,5 +1,5 @@
 int	ft_write(int fd, const void* buf, int count);
 int main(void){
-	ft_write(1, "oui", 3);
+	ft_write(1, "oui\n", 4);
 	return 0;
 }
