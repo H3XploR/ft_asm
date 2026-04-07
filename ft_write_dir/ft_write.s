e00178cd16b88a27cc36e6e187a934dbac5b81d2
hexplor <hexplor@hexplor.com>
2026-04-07 05:23:06 +0200

    alignement de la stack mauvaise

--- ft_write_dir/ft_write.s
+++ ft_write_dir/ft_write.s
@@ -6,3 +6,15 @@ section .text
 extern __errno_location
 global ft_write
 
+ft_write:
+	mov	rax, 1
+	syscall
+	cmp	rax, 0
+	jge	end
+	mov	r8, rax
+	call	__errno_location WRT ..plt
+	neg	r8
+	mov	[rax], r8
+	ret
+end:
+	ret
--- ft_write_dir/test.c
+++ ft_write_dir/test.c
@@ -1,5 +1,22 @@
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+
 int	ft_write(int fd, const void* buf, int count);
-int main(void){
-	ft_write(1, "oui\n", 4);
-	return 0;
-}
+
+
+
+int main(void) {
+    int fd = 99;  // fd invalide
+    const char *msg = "hello\n";
+
+    ssize_t ret =  ft_write(1, "oui\n", 4); //appel_valide
+    
+    ret = ft_write(fd, msg, 6); //appel_err
+    if (ret < 0) {
+        printf("erreur : %s (errno = %d)\n", strerror(errno), errno);
+    }
+
+    return 0;
+} 
