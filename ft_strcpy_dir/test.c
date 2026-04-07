3281922f4f6e3fa9bf2128828b6184630354d96a
hexplor <hexplor@hexplor.com>
2026-04-07 23:45:54 +0200

    commented test NULL

--- ft_strcpy_dir/test.c
+++ ft_strcpy_dir/test.c
@@ -6,7 +6,11 @@ int main(void) {
 	char msg[6] = "voila";
 	char dest[6] = "12345";
 	printf("before cpy: %s\n", dest);
-	char* new_string = ft_strcpy(dest, msg);
+	ft_strcpy(dest, msg);
 	printf("after cpy: %s\n", dest);
+	//printf("test avec NULL\n");
+	//char* null_str = NULL;
+	//ft_strcpy(dest, null_str);
+	//ft_strcpy(null_str, dest);
 	return 0;
 }
