625367147a05fa267d1736b36bf144c462e03192
hexplor <hexplor@hexplor.com>
2026-04-08 14:54:24 +0200

    MAJ

--- ft_atoi_base_dir/ft_atoi_base.s
+++ ft_atoi_base_dir/ft_atoi_base.s
@@ -1,16 +1,9 @@
-;ft_strlen.s
+;ft_atoi_base.s
 BITS 64; pour specifier automatiquement a NASM que je veux les instruction en 64bits
 section .note.GNU-stack noalloc noexec nowrite progbits
 
 section .text		; start of code segment
-	global ft_strlen; declare _start to be visible in the generated object file
+	global ft_atoi_base; declare _start to be visible in the generated object file
 
-ft_strlen:
-	xor	rax, rax ; initialiser le compteur a 0
-.loop:
-	cmp	byte [rdi + rax], 0; comparer
-	jz	.end
-	inc	rax
-	jmp	.loop
-.end:
-	ret
+ft_atoi_base:
+	
--- ft_atoi_base_dir/test.c
+++ ft_atoi_base_dir/test.c
@@ -3,7 +3,7 @@
 int	ft_atoi_base(char *str, char *base);
 
 int	main(void){
-	const char msg[6] = "53";
+	char msg[6] = "53";
 	int numb = ft_atoi_base(msg, "0123456789");
 	printf("avec \"%s\", atoi_base a returner le numero: %d en base 10\n", msg, numb);
 	return 0;
