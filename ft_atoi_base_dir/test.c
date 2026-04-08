a9588000f4a563a0807ff629dd86255212326ba1
hexplor <hexplor@hexplor.com>
2026-04-08 21:55:30 +0200

    adding more test for atoi_base

--- ft_atoi_base_dir/test.c
+++ ft_atoi_base_dir/test.c
@@ -4,7 +4,13 @@ int	ft_atoi_base(char *str, char *base);
 
 int	main(void){
 	char msg[6] = "53";
-	int numb = ft_atoi_base(msg, "0123456789");
+	char base_10[10] = "0123456789";
+	int numb = ft_atoi_base(msg, base_10);
 	printf("avec \"%s\", atoi_base a returner le numero: %d en base 10\n", msg, numb);
+
+
+	char out_base[2] = "a";
+	printf("test avec character (%s) qui n'appartient pas a la base (%s)\n", out_base, base_10);
+	numb = ft_atoi_base(out_base, base_10);
 	return 0;
 }
