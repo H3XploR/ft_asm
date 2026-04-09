2dd641694978946ce6714f8d22ef3be42d463849
hexplor <hexplor@hexplor.com>
2026-04-09 02:27:27 +0200

    update test.c pour nouveau test

--- ft_atoi_base_dir/test.c
+++ ft_atoi_base_dir/test.c
@@ -6,11 +6,24 @@ int	main(void){
 	char msg[6] = "53";
 	char base_10[11] = "0123456789";
 	int numb = ft_atoi_base(msg, base_10);
-	printf("avec \"%s\", atoi_base a returner le numero: %d en base 10\n", msg, numb);
+	printf("avec \"%s\", atoi_base a returner le numero: %d en base 10\n\n", msg, numb);
 
 
 	char out_base[2] = "a";
 	printf("test avec character (%s) qui n'appartient pas a la base (%s)\n", out_base, base_10);
-	numb = ft_atoi_base(out_base, base_10);
+	numb = ft_atoi_base(out_base, base_10); printf("numb = %d\n\n", numb);
+
+
+	printf("test avec une chaine vide en premier argument\n");
+	numb = ft_atoi_base("", base_10); printf("numb = %d\n\n", numb);
+
+	printf("test avec une chaine vide en deuxieme argument\n");
+	numb = ft_atoi_base("", base_10); printf("numb = %d\n\n", numb);
+
+	printf("test avec deux chaine vide en  argument\n");
+	numb = ft_atoi_base("", ""); printf("numb = %d\n\n", numb);
+
+	printf("test avec une valeur negative\n");
+	numb = ft_atoi_base("-8", "0123456789"); printf("numb = %d\n\n", numb);
 	return 0;
 }
